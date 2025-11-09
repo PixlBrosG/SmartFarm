463b51453239f687e8aa30881dd14b2636cbe268
Pixl <pixelbrosg@gmail.com>
2025-11-09 18:11:25 +0100

    feat(network): Tested simple HELLO -> ACK

--- src/Sensor/Main.cpp
+++ src/Sensor/Main.cpp
@@ -1,8 +1,40 @@
+#include <asio.hpp>
+
+#include "SmartFarm/Message.h"
 #include "SmartFarm/Logger.h"
+
+using asio::ip::tcp;
 
 int main()
 {
 	SmartFarm::Logger::init();
+	SmartFarm::Logger::info("Starting Sensor Node...");
 
-	SmartFarm::Logger::info("Starting Sensor...");
+	try
+	{
+		asio::io_context io;
+		tcp::socket socket(io);
+		socket.connect({asio::ip::make_address("127.0.0.1"), SmartFarm::Protocol::DEFAULT_PORT });
+
+		// Create HELLO message
+		SmartFarm::Message msg;
+		msg.Type = SmartFarm::Protocol::MessageType::HELLO;
+		msg.Payload = { { "node_id", 1 }, { "role", "sensor" } };
+
+		auto data = msg.Serialize();
+		asio::write(socket, asio::buffer(data));
+
+		SmartFarm::Logger::info("HELLO sent.");
+
+		// Wait for ACK
+		std::array<char, 1024> buffer{};
+		size_t len = socket.read_some(asio::buffer(buffer));
+		std::string received(buffer.data(), len);
+
+		SmartFarm::Logger::info("Received: {}", received);
+	}
+	catch (std::exception& e)
+	{
+		SmartFarm::Logger::error("Client error: {}", e.what());
+	}
 }
--- src/Server/Main.cpp
+++ src/Server/Main.cpp
@@ -1,8 +1,50 @@
+#include <asio.hpp>
+
 #include "SmartFarm/Logger.h"
+#include "SmartFarm/Message.h"
+
+using asio::ip::tcp;
 
 int main()
 {
 	SmartFarm::Logger::init();
+	SmartFarm::Logger::info("Starting SmartFarm Server...");
 
-	SmartFarm::Logger::info("Starting Server...");
+	try
+	{
+		asio::io_context io;
+		tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), SmartFarm::Protocol::DEFAULT_PORT));
+
+		while (true)
+		{
+			tcp::socket socket(io);
+			acceptor.accept(socket);
+			SmartFarm::Logger::info("Client connected!");
+
+			std::array<char, 1024> buffer{};
+			size_t len = socket.read_some(asio::buffer(buffer));
+
+			std::string received(buffer.data(), len);
+			auto msg = SmartFarm::Message::Deserialize(received);
+
+			SmartFarm::Logger::info("Received: {}", received);
+
+			// Respond with ACK
+			SmartFarm::Message reply;
+			reply.Type = SmartFarm::Protocol::MessageType::ACK;
+			reply.Payload = {{ "message", "Registered node" }};
+
+			auto response = reply.Serialize();
+			asio::write(socket, asio::buffer(response));
+
+			SmartFarm::Logger::info("ACK sent.");
+		}
+	}
+	catch (asio::system_error& e)
+	{
+		if (e.code() == asio::error::eof)
+			SmartFarm::Logger::warn("Client disconnected normally (EOF)");
+		else
+			SmartFarm::Logger::error("Server error: {}", e.what());
+	}
 }
